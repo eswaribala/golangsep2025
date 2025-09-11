feb8dc68436b2425a222b5d768d05f8bb4f2358a
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-09-11 18:40:16 +0530

    updated

--- claimapp/models/vehicle.go
+++ claimapp/models/vehicle.go
@@ -1,18 +1,22 @@
 package models
+
+import "time"
+
 //custom type
 type FuelType string
 
 const (
-	Petrol FuelType = "petrol"
-	Diesel FuelType = "diesel"
+	Petrol   FuelType = "petrol"
+	Diesel   FuelType = "diesel"
 	Electric FuelType = "electric"
 )
 
 type Vehicle struct {
-	RegistrationNo string   `json:"id"`
-	Maker         string `json:"make"`
-	ChassisNo        string `json:"chassis_no"`
-	FuelType        FuelType `json:"fuel_type"`
-	EngineNo        string `json:"engine_no"`
-	Color        string `json:"color"`
+	RegistrationNo     string    `json:"id"`
+	Maker              string    `json:"make"`
+	DateOfRegistration time.Time `json:"date_of_registration"`
+	ChassisNo          string    `json:"chassis_no"`
+	FuelType           FuelType  `json:"fuel_type"`
+	EngineNo           string    `json:"engine_no"`
+	Color              string    `json:"color"`
 }
