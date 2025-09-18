4b6432e4a5aa4680d4ae9f089bc6bca747f7203c
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-09-18 19:19:49 +0530

    updated

--- claimapp/gocharts/store/claim.go
+++ claimapp/gocharts/store/claim.go
@@ -22,28 +22,51 @@ func GenerateBarGraphClaims(claims []Claim, models []string) {
 	for _, c := range claims {
 		counts[c.ModelName]++
 	}
+	/*
+		// Prepare chart data
+		var xAxis []string
+		var yAxis []opts.BarData
+		for _, m := range models {
+			xAxis = append(xAxis, m)
+			yAxis = append(yAxis, opts.BarData{Value: counts[m]})
+		}
 
-	// Prepare chart data
-	var xAxis []string
-	var yAxis []opts.BarData
+			// Create bar chart
+			bar := charts.NewBar()
+			bar.SetGlobalOptions(
+				charts.WithTitleOpts(opts.Title{
+					Title:    "Claims by Models",
+					Subtitle: "Generated using faker & random",
+				}),
+			)
+			bar.SetXAxis(xAxis).AddSeries("Claims", yAxis)
+	*/
+
+	//prepaere pie chart data
+	var pieData []opts.PieData
 	for _, m := range models {
-		xAxis = append(xAxis, m)
-		yAxis = append(yAxis, opts.BarData{Value: counts[m]})
+		pieData = append(pieData, opts.PieData{Name: m, Value: counts[m]})
 	}
 
-	// Create bar chart
-	bar := charts.NewBar()
-	bar.SetGlobalOptions(
+	//create pie chart
+	pie := charts.NewPie()
+	pie.SetGlobalOptions(
 		charts.WithTitleOpts(opts.Title{
 			Title:    "Claims by Models",
 			Subtitle: "Generated using faker & random",
 		}),
 	)
-	bar.SetXAxis(xAxis).AddSeries("Claims", yAxis)
+
+	pie.AddSeries("Claims", pieData).SetSeriesOptions(
+
+		charts.WithPieChartOpts(opts.PieChart{
+			Radius: []string{"40%", "70%"},
+		}),
+	)
 
 	// Save to HTML
 	f, _ := os.Create("claims.html")
 	defer f.Close()
-	bar.Render(f)
+	pie.Render(f)
 
 }
