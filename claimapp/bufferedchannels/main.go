b8acebcc83cd52bc4fbcad78f237cc21a9b5c7a5
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-09-22 19:29:22 +0530

    updated

---
+++ claimapp/bufferedchannels/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/brianvoe/gofakeit/v7"
+	"github.com/eswaribala/claimapp/models"
+)
+
+func main() {
+
+	claimChannel := make(chan models.Claim, 5)
+
+	//claim array
+	var claims []models.Claim
+	//simulate adding claims to the channel
+	for i := 1; i <= 10; i++ {
+		claim := models.Claim{
+			ID:     uint(gofakeit.IntRange(10000, 10000000)),
+			Amount: gofakeit.IntRange(10000, 10000000),
+		}
+		claims = append(claims, claim)
+
+	}
+
+	go func() {
+		//claim raised by the client
+		println("Raising claims...")
+		count := 0
+		for _, claim := range claims {
+			claimChannel <- claim
+			count++
+			println("Claims raised:", count)
+		}
+
+	}()
+
+	//claim processed by the server
+	println("Processing claims...")
+	for i := 1; i <= 10; i++ {
+		claim := <-claimChannel
+		println("Claim ID:", claim.ID, "Amount:", claim.Amount)
+	}
+
+}
