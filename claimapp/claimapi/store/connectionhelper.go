2ac7598be1fb79a742f1f2861c3d87c5006191b3
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-09-25 18:13:13 +0530

    updated

---
+++ claimapp/claimapi/store/connectionhelper.go
@@ -0,0 +1,35 @@
+package store
+
+import (
+	"context"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+
+func MongoDBConnectionHelper() (*mongo.Client, error) {
+	// MongoDB connection helper
+	_ = godotenv.Load(".env")
+	uri := os.Getenv("uri")
+	// Placeholder for MongoDB connection and insertion logic
+	//timeout context
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// MongoDB connection URI
+	clientOptions := options.Client().ApplyURI(uri)
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, err
+	}
+	//defer client.Disconnect(ctx)
+	client.Database("ClaimDB").Collection("claims")
+	return client, nil
+
+}
