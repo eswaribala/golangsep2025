8e9e6532cef7c83dd993ab27d3cb8a35d9541a64
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-09-25 18:30:47 +0530

    updated

--- claimapp/claimapi/store/connectionhelper.go
+++ claimapp/claimapi/store/connectionhelper.go
@@ -2,6 +2,8 @@ package store
 
 import (
 	"context"
+	"encoding/json"
+	"net/http"
 	"os"
 	"time"
 
@@ -30,4 +32,108 @@ func MongoDBConnectionHelper() (*mongo.Client, error) {
 	client.Database("ClaimDB").Collection("claims")
 	return client, nil
 
+}
+
+// crud
+func SaveClaim(writer http.ResponseWriter, request *http.Request) {
+	// Placeholder for save claim logic
+	mongoClient, err := MongoDBConnectionHelper()
+	if err != nil {
+		writer.Write([]byte("Error connecting to MongoDB"))
+		return
+	}
+
+	//request to db
+	collection := mongoClient.Database("ClaimDB").Collection("claims")
+	var claim Claim
+	json.NewDecoder(request.Body).Decode(&claim)
+	_, err = collection.InsertOne(context.TODO(), claim)
+	writer.Header().Set("Content-Type", "application/json")
+
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(writer).Encode(claim)
+
+}
+
+func GetClaims(writer http.ResponseWriter, request *http.Request) {
+	// Placeholder for get claims logic
+	mongoClient, err := MongoDBConnectionHelper()
+	if err != nil {
+		writer.Write([]byte("Error connecting to MongoDB"))
+		return
+	}
+	collection := mongoClient.Database("ClaimDB").Collection("claims")
+	cursor, err := collection.Find(context.TODO(), struct{}{})
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	var claims []Claim
+	if err = cursor.All(context.TODO(), &claims); err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	writer.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(claims)
+
+}
+
+func GetClaimByID(writer http.ResponseWriter, request *http.Request) {
+	// Placeholder for get claim by ID logic
+	mongoClient, err := MongoDBConnectionHelper()
+	if err != nil {
+		writer.Write([]byte("Error connecting to MongoDB"))
+		return
+	}
+	collection := mongoClient.Database("ClaimDB").Collection("claims")
+	//get claim by id
+	var claim Claim
+	id := request.URL.Query().Get("id")
+	err = collection.FindOne(context.TODO(), map[string]interface{}{"id": id}).Decode(&claim)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	writer.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(claim)
+
+}
+
+func UpdateClaim(writer http.ResponseWriter, request *http.Request) {
+	mongoClient, err := MongoDBConnectionHelper()
+	if err != nil {
+		writer.Write([]byte("Error connecting to MongoDB"))
+		return
+	}
+	collection := mongoClient.Database("ClaimDB").Collection("claims")
+	var claim Claim
+	json.NewDecoder(request.Body).Decode(&claim)
+	_, err = collection.UpdateOne(context.TODO(), map[string]interface{}{"id": claim.ID}, map[string]interface{}{"$set": claim})
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	writer.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(claim)
+
+}
+
+func DeleteClaim(writer http.ResponseWriter, request *http.Request) {
+	// Placeholder for delete claim logic
+	mongoClient, err := MongoDBConnectionHelper()
+	if err != nil {
+		writer.Write([]byte("Error connecting to MongoDB"))
+		return
+	}
+	collection := mongoClient.Database("ClaimDB").Collection("claims")
+	id := request.URL.Query().Get("id")
+	_, err = collection.DeleteOne(context.TODO(), map[string]interface{}{"id": id})
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	writer.WriteHeader(http.StatusNoContent)
 }
