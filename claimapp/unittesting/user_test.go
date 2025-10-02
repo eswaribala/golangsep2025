0e730205da628d971e2507bbf02415c8bf562cdb
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-10-02 19:37:30 +0530

    updated

--- claimapp/unittesting/user_test.go
+++ claimapp/unittesting/user_test.go
@@ -51,3 +51,99 @@ func TestUserFromCSV(t *testing.T) {
 		assert.NoError(t, err, "Validation failed for user: %+v", user)
 	}
 }
+
+func TestUserNameFromCSVAssertEqual(t *testing.T) {
+	csvReader, err := OpenCSVFile("userdata.csv")
+	if err != nil {
+		t.Fatalf("Failed to open CSV file: %v", err)
+	}
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		t.Fatalf("Failed to read CSV file: %v", err)
+	}
+	for _, record := range records[1:] {
+		user := User{
+			Name:     record[0],
+			Email:    record[1],
+			Password: record[2],
+		}
+		log.Println("Testing user:", user)
+		tests := []struct {
+			name string
+			in   User
+			want error
+		}{
+			{"Valid User Name", user, ErrInvalidUserName},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				err := ValidateUser(tt.in)
+				assert.Equal(t, tt.want, err)
+			})
+		}
+	}
+}
+
+func TestUserEmailFromCSVAssertEqual(t *testing.T) {
+	csvReader, err := OpenCSVFile("userdata.csv")
+	if err != nil {
+		t.Fatalf("Failed to open CSV file: %v", err)
+	}
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		t.Fatalf("Failed to read CSV file: %v", err)
+	}
+	for _, record := range records[1:] {
+		user := User{
+			Name:     record[0],
+			Email:    record[1],
+			Password: record[2],
+		}
+		log.Println("Testing user:", user)
+		tests := []struct {
+			name string
+			in   User
+			want error
+		}{
+			{"Valid User Name", user, ErrInvalidUserEmail},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				err := ValidateUser(tt.in)
+				assert.Equal(t, tt.want, err)
+			})
+		}
+	}
+}
+
+func TestUserPasswordFromCSVAssertEqual(t *testing.T) {
+	csvReader, err := OpenCSVFile("userdata.csv")
+	if err != nil {
+		t.Fatalf("Failed to open CSV file: %v", err)
+	}
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		t.Fatalf("Failed to read CSV file: %v", err)
+	}
+	for _, record := range records[1:] {
+		user := User{
+			Name:     record[0],
+			Email:    record[1],
+			Password: record[2],
+		}
+		log.Println("Testing user:", user)
+		tests := []struct {
+			name string
+			in   User
+			want error
+		}{
+			{"Valid User Name", user, ErrInvalidUserPass},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				err := ValidateUser(tt.in)
+				assert.Equal(t, tt.want, err)
+			})
+		}
+	}
+}
